4bdfaa9235b064cf4fc261f9bcb0adb602e15201
Sean Nguyen <seannguyen@seans-mac-mini.home>
2023-10-26 14:11:02 +1100

    first commit

---
+++ TopInterview150/26_RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    # def removeDuplicates(self, nums: List[int]) -> int:
+    #     unique_element = 0
+    #     dictionary_element = {}
+    #     for i in range(0, len(nums)):
+    #         if nums[i] in dictionary_element:
+    #             dictionary_element[nums[i]]['counter'] += 1
+    #         else:
+    #             unique_element += 1
+    #             dictionary_element[nums[i]] = {
+    #                 'counter': 1,
+    #                 'index': i
+    #             }
+
+    #     for i in range(0, unique_element-1):
+    #         next_unique_occurence = dictionary_element[nums[i]]['index'] + dictionary_element[nums[i]]['counter']
+    #         nums[i+1] = nums[next_unique_occurence]
+    #         i += 1
+    #     return unique_element
+    
+    def removeDuplicates(self, nums: List[int]) -> int:
+        duplicates = 0
+        for i in range(1, len(nums)):
+            if(nums[i] == nums[i-1]): duplicates+=1
+            else: nums[i-duplicates] = nums[i]
+        return len(nums)-duplicates
+        
+
+if __name__ == "__main__":
+    
+    # this won't work because Solution.merge is not a static method
+    # Solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+
+    solution = Solution()
+    solution.removeDuplicates([0,0,1,1,1,2,2,3,3,4])
---
+++ TopInterview150/88_MergeSortedArray.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = 0
+        j = 0
+        while(i < m and j < n):
+            if nums2[j] <= nums1[i]:
+                nums1.insert(i+1, nums2[j])
+                j+=1
+            i+=1
+        print(nums1)
+        if j < n:
+            nums1 += nums2[j:]
+        
+        print(nums1)
+
+if __name__ == "__main__":
+    
+    # this won't work because Solution.merge is not a static method
+    # Solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+
+    solution = Solution()
+    solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
